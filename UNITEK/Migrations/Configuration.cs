4bfcedcc10468a82d7074eabe2c4a5b57db28224
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2018-07-30 08:40:52 -0500

    Using Entity Framework

---
+++ UNITEK/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace UNITEK.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UNITEK.BaseDatos.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(UNITEK.BaseDatos.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ UNITEK/Modelos/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNITEK.Modelos
+{
+    [Table("Alumnos")] //Anotación para dar nombre a nuestra tabla al realizar la migración
+    public class Alumno
+    {
+        [Key] //Anotación para establecer la llave primaria a nuestra tabla
+        [Column("id_alumno")] //Anotación para dar nombre a una columna de nuestra tabla
+        [Required] //Anotación que obliga a no tener valores nulos
+        public int IdAlumno  { get; set; }
+
+        [Column("nombres")]
+        [Required]
+        [StringLength(50)] //Anotación permite establecer una cadena con valor máximo de 50 carácteres
+        public string Nombres { get; set; }
+
+        [Column("apellidos")]
+        [Required]
+        [StringLength(50)]
+        public string Apellidos { get; set; }
+    }
+}
